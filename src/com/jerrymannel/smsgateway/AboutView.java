2e7b15ee28454a47c8cf98b462f3e0b78a7b0164
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-06-22 00:12:56 +0200

    Fix bad querystring parsing which was causing crashes

--- src/com/jerrymannel/smsgateway/AboutView.java
+++ src/com/jerrymannel/smsgateway/AboutView.java
@@ -1,7 +1,5 @@
 package com.jerrymannel.smsgateway;
 
-import com.jerrymannel.smsgateway.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -17,7 +15,7 @@ public class AboutView extends Activity {
 		textView2.setText("Open the app and start the server. The app displays the URL to which the request has to be made.\n\n" + 
 						"The HTTP GET URL is as shown below,\n\n" + 
 						"http://<device ip>:18080/?<phone number>=<short message>\n\n" + 
-						"e.g. http://192.168.2.3:18080/?phone=+919912345678&message=HelloWorld\n\n" +
+						"e.g. http://192.168.2.3:18080/?phone=%2B919912345678&message=HelloWorld\n\n" +
 						"The app MUST be running in the foreground to access the service. If the app closes or goes into the background, the server stops automatically.");
 	}
 }
